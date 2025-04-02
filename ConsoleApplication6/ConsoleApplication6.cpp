dd622d047b473fe0ec241d3704a57fd513ce02ac
Christopher Wilson <ckw82076@email.vccs.edu>
2025-04-02 08:13:08 -0400

    Update ConsoleApplication6.cpp

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -2,19 +2,35 @@
 Programmer/Name: Christopher Wilson
 Date: 3/26/2025
 
-Requirements: Create a program that calculates the number of miles per gallon a car can get with 15 gallons of gasoline then display the results.
+Requirements: (Part 2) Create a program hat calculates the sales tax of a custom amount purchase as well as a custom state and county tax
 */
 
 #include <iostream>
+#include <iomanip>
 
 int main() {
 
-	double gallonsOfGas = 15.0;
-	double MilesDriven = 375.0;
+	double purchaseAmount, stateTaxR, countyTaxR;
 
-	double mpg = MilesDriven / gallonsOfGas;
+	std::cout << "Enter the purchase amount you've made: $";
+	std::cin >> purchaseAmount;
+	std::cout << "Enter the state sales tax amount (as a percentage amount): ";
+	std::cin >> stateTaxR;
+	std::cout << "Enter the county sales tax amount (as a percentage amount): ";
+	std::cin >> countyTaxR;
 
-	std::cout << "The car gets " << mpg << " miles per gallon." << std::endl;
+	stateTaxR /= 100.0;
+	countyTaxR /= 100.0;
+
+	double stateTax = purchaseAmount * stateTaxR;
+	double countyTax = purchaseAmount * countyTaxR;
+	double totalTax = stateTax + countyTax;
+
+	std::cout << std::fixed << std::setprecision(2);
+	std::cout << "Purchase Amount: $" << purchaseAmount << std::endl;
+	std::cout << "State Sales Tax: $" << stateTax << std::endl;
+	std::cout << "County Sales Tax: $" << countyTax << std::endl;
+	std::cout << "Total Sales Tax: $" << totalTax << std::endl;
 
 	return 0;
 }
