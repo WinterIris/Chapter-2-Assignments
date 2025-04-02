6545906383c02513131698fc71b07cc25c57743c
Christopher Wilson <ckw82076@email.vccs.edu>
2025-04-02 08:05:00 -0400

    Update ConsoleApplication6.cpp

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -2,19 +2,27 @@
 Programmer/Name: Christopher Wilson
 Date: 3/26/2025
 
-Requirements: Create a program that calculates the number of miles per gallon a car can get with 15 gallons of gasoline then display the results.
+Requirements: (Part 1) Create a program hat calculates the sales tax of a $95 purchase, assuming state is 4% and county is 2%
 */
 
 #include <iostream>
+#include <iomanip>
 
 int main() {
 
-	double gallonsOfGas = 15.0;
-	double MilesDriven = 375.0;
+	const double purchaseAmount = 95.0;
+	const double stateTaxR = 0.04;
+	const double countyTaxR = 0.02;
 
-	double mpg = MilesDriven / gallonsOfGas;
+	double stateTax = purchaseAmount * stateTaxR;
+	double countyTax = purchaseAmount * countyTaxR;
+	double totalTax = stateTax + countyTax;
 
-	std::cout << "The car gets " << mpg << " miles per gallon." << std::endl;
+	std::cout << std::fixed << std::setprecision(2);
+	std::cout << "Purchase Amount: $" << purchaseAmount << std::endl;
+	std::cout << "State Sales Tax: $" << stateTax << std::endl;
+	std::cout << "County Sales Tax: $" << countyTax << std::endl;
+	std::cout << "Total Sales Tax: $" << totalTax << std::endl;
 
 	return 0;
 }
