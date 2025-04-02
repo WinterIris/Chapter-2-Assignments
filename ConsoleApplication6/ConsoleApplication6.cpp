3301a504173e860ead3ddb5568d85fd5358f5d93
Christopher Wilson <ckw82076@email.vccs.edu>
2025-04-02 08:47:35 -0400

    Update ConsoleApplication6.cpp

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,20 +1,29 @@
 /* File Name: Chapter 2 Assignment
 Programmer/Name: Christopher Wilson
-Date: 3/26/2025
+Date: 3/27/2025
 
-Requirements: Create a program that calculates the number of miles per gallon a car can get with 15 gallons of gasoline then display the results.
+Requirements: (Part 2) Create a program that allows the users input for the size of the fence.
 */
 
 #include <iostream>
+#include <cmath>
 
 int main() {
 
-	double gallonsOfGas = 15.0;
-	double MilesDriven = 375.0;
+	double fenceH, fenceL;
+	const double coveragePerGallon = 340.0;
+	const int coats = 2;
 
-	double mpg = MilesDriven / gallonsOfGas;
+	std::cout << "What is the height of the fence that needs to be painted?";
+	std::cin >> fenceH;
+	std::cout << "What is the length of the fence that needs to be painted?";
+	std::cin >> fenceL;
 
-	std::cout << "The car gets " << mpg << " miles per gallon." << std::endl;
+	double totalArea = fenceH * fenceL * coats;
+	double gallonsNeeded = std::ceil(totalArea / coveragePerGallon);
+
+	std::cout << "Total area needed to be painted (2 Coats mentioned): " << totalArea << " square feet\n";
+	std::cout << "Gallons of paint needed: " << gallonsNeeded << " gallons\n";
 
 	return 0;
 }
